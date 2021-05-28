904a62267242f4042067110a95f68e6a0ac4066d
Michael Ding <yandy.ding@gmail.com>
2021-05-28 13:59:51 +0800

    spicmd for update

--- include/sd/robot/interface.h
+++ include/sd/robot/interface.h
@@ -61,7 +61,7 @@ namespace sd::robot
   class Interface
   {
   public:
-    const SPICmd& GetSPICmd() const { return spi_cmd_; }
+    SPICmd& GetSPICmdForUpdate() { return spi_cmd_; }
     const SPIData& GetSPIData() const { return spi_data_; }
     const IMUData& GetIMUData() const { return imu_data_; }
     virtual bool Init() = 0;   // return true if ok
