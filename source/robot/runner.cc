a971e6cf8b05229f83fcc4d36f3ea14e18a64e45
Michael Ding <yandy.ding@gmail.com>
2021-06-10 14:36:34 +0800

    add endtime for jpos init

--- source/robot/runner.cc
+++ source/robot/runner.cc
@@ -28,7 +28,7 @@ namespace sd::robot
 
     // init ctrls
     leg_ctrl_ = std::make_unique<ctrl::LegCtrl>();
-    jpos_ctrl_ = std::make_unique<ctrl::JPosInit>();
+    jpos_ctrl_ = std::make_unique<ctrl::JPosInit>(3.); //endtime = 3.0
 
     // init state estimator
     // TODO initializeStateEstimator
