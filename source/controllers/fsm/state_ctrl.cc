d53c83d92111260584b9b48ade64d79369ebe35b
Michael Ding <yandy.ding@gmail.com>
2021-07-24 18:47:02 +0800

    new struct

--- source/controllers/fsm/state_ctrl.cc
+++
@@ -1,8 +0,0 @@
-#include "sdrobot/controllers/fsm.h"
-
-namespace sdrobot::ctrl::fsm
-{
-  StateCtrl::StateCtrl(const LegPtr &cleg, const robot::QuadrupedPtr &quad, const StateCmdPtr &cmd, const est::StateEstPtr &est)
-      : leg_ctrl_(cleg), quad_(quad), state_cmd_(cmd), state_est_(est) {}
-
-}
