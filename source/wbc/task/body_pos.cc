82622b7edde179253d98401fc94c9dee890b7924
Michael Ding <yandy.ding@gmail.com>
2021-08-03 00:20:27 +0800

    add body pos

---
+++ source/wbc/task/body_pos.cc
@@ -0,0 +1,66 @@
+#include "dynamics/rotation.h"
+
+#include "wbc/task/body_pos.h"
+
+namespace sdrobot::wbc
+{
+  TaskBodyPos::TaskBodyPos(
+      model::FloatBaseModel::SharedPtr const &model,
+      SdVector3f const &kp, SdVector3f const &kd) : Task(model)
+  {
+    Jt_.fill(0.);
+    Eigen::Map<Jt_t> Jt(Jt_.data());
+    Jt.block<3, 3>(0, 0).setIdentity();
+
+    JtDotQdot_.fill(0.);
+
+    Kp_kin_.fill(1.);
+    Kp_ = kp;
+    Kd_ = kd;
+  }
+
+  // Update op_cmd_
+  bool TaskBodyPos::_UpdateCommand(SdVector3f const &pos_des, SdVector3f const &vel_des,
+                                   SdVector3f const &acc_des)
+  {
+    auto const &robot_pos =robot_sys_->GetState().pos;
+    dynamics::RotMat rot;
+    dynamics::QuatToRotMat(rot, ToConstEigenTp(robot_sys_->GetState().ori));
+    Vector6 curr_vel = ToConstEigenTp(robot_sys_->GetState().vel);
+    curr_vel.tail(3) = rot.transpose() * curr_vel.tail(3);
+
+    // X, Y, Z
+    for (int i(0); i < 3; ++i)
+    {
+      pos_err_[i] = Kp_kin_[i] * (pos_des[i] - robot_pos[i]);
+      vel_des_[i] = vel_des[i];
+      acc_des_[i] = acc_des[i];
+
+      op_cmd_[i] = Kp_[i] * (pos_des[i] - robot_pos[i]) +
+                   Kd_[i] * (vel_des_[i] - curr_vel[i + 3]) +
+                   acc_des_[i];
+    }
+
+    return true;
+  }
+
+  bool TaskBodyPos::_UpdateTaskJacobian()
+  {
+    dynamics::RotMat rot;
+    dynamics::QuatToRotMat(rot, ToConstEigenTp(robot_sys_->GetState().ori));
+    Eigen::Map<Jt_t> Jt(Jt_.data());
+    Jt.block<3, 3>(0, 0) = rot.transpose();
+    return true;
+  }
+
+  bool TaskBodyPos::_UpdateTaskJDotQdot()
+  {
+    return true;
+  }
+
+  bool TaskBodyPos::_AdditionalUpdate()
+  {
+    return true;
+  }
+
+}
---
+++ source/wbc/task/body_pos.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "wbc/task.h"
+
+namespace sdrobot::wbc
+{
+  class TaskBodyPos : public Task
+  {
+  public:
+    TaskBodyPos(model::FloatBaseModel::SharedPtr const &model, SdVector3f const &kp, SdVector3f const &kd);
+
+  private:
+    // Update op_cmd_
+    bool _UpdateCommand(SdVector3f const &pos_des, SdVector3f const &vel_des,
+                        SdVector3f const &acc_des);
+    // Update Jt_
+    bool _UpdateTaskJacobian();
+    // Update JtDotQdot_
+    bool _UpdateTaskJDotQdot();
+    // Additional Update (defined in child classes)
+    bool _AdditionalUpdate();
+
+    SdVector3f Kp_kin_, Kp_, Kd_;
+  };
+
+}
