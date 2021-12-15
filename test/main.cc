bf11dccbfe5ccc6b4d15708bc6d44e69b35bb24f
Micheal Ding <yandy.ding@gmail.com>
2021-12-15 20:40:40 +0800

    add log for test

--- test/main.cc
+++ test/main.cc
@@ -1,8 +1,12 @@
+#include <iostream>
+
 namespace sdquadx::test {
 void RunExample();
 }  // namespace sdquadx::test
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
+  std::cout << "Start Test!!!" << std::endl;
   sdquadx::test::RunExample();
+  std::cout << "End Test!!!" << std::endl;
   return 0;
 }
