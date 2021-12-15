96ab0621d6c5bbcf6f0e33e3e0ea4f3f18552d58
Micheal Ding <yandy.ding@gmail.com>
2021-12-15 10:51:42 +0800

    fix example

--- test/example.cc
+++ test/example.cc
@@ -9,19 +9,15 @@ namespace sdquadx::test {
 int ctrl_iter = 0;
 
 void RunRobot(RobotCtrl::Ptr const &robot, std::shared_ptr<interface::LegImpl> const &legitf,
-              std::shared_ptr<interface::ImuImpl> const &imuitf, int const ctrl_dt, int const itf_dt,
-              int const total_dt) {
+              std::shared_ptr<interface::ImuImpl> const &imuitf, int const ctrl_dt, int const total_dt) {
   for (int i = 0; i < total_dt; i++) {
     if (i % ctrl_dt == 0) {
       robot->RunOnce();
       ctrl_iter++;
       printf("!!!![Iteration %d]\n", ctrl_iter);
     }
-
-    if (i % itf_dt == 0) {
-      legitf->RunOnce();
-      imuitf->RunOnce();
-    }
+    legitf->RunOnce();
+    imuitf->RunOnce();
   }
 }
 
@@ -32,7 +28,6 @@ void RunExample() {
   opts->log_level = "debug";
 
   int ctrl_dt = 1000 * opts->ctrl_sec;
-  int itf_dt = 25;
 
   auto leg_itf = std::make_shared<interface::LegImpl>();
   auto imu_itf = std::make_shared<interface::ImuImpl>();
@@ -43,11 +38,11 @@ void RunExample() {
   auto drive_ctrl = robot->GetDriveCtrl();
 
   // State Init
-  RunRobot(robot, leg_itf, imu_itf, ctrl_dt, itf_dt, 10'000);
+  RunRobot(robot, leg_itf, imu_itf, ctrl_dt, 10'000);
 
   // State Recovery Stand
   drive_ctrl->UpdateState(drive::State::RecoveryStand);
-  RunRobot(robot, leg_itf, imu_itf, ctrl_dt, itf_dt, 10'000);
+  RunRobot(robot, leg_itf, imu_itf, ctrl_dt, 10'000);
 
   // State Locomotion
   drive_ctrl->UpdateMode(drive::Mode::Manual);
@@ -55,7 +50,7 @@ void RunExample() {
   drive_ctrl->UpdateState(drive::State::Locomotion);
   drive::Twist drive_twist;
   drive_ctrl->UpdateTwist(drive_twist);
-  RunRobot(robot, leg_itf, imu_itf, ctrl_dt, itf_dt, 20'000);
+  RunRobot(robot, leg_itf, imu_itf, ctrl_dt, 20'000);
 }
 
 }  // namespace sdquadx::test
