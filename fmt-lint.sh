6e5dbb4b683da272f60e7f532c86e7b3e9a89ae4
Michael Ding <yandy.ding@gmail.com>
2022-02-16 00:01:19 +0800

    there is no .cc in include

--- fmt-lint.sh
+++ fmt-lint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 find $(pwd)/include -name '*.h' -exec clang-format -i {} \;
-find $(pwd)/include -name '*.cc' -exec clang-format -i {} \;
 
 find $(pwd)/source -name '*.h' -exec clang-format -i {} \;
 find $(pwd)/source -name '*.cc' -exec clang-format -i {} \;
