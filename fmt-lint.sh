a749c9ca28b12d122e1a3719dd714147343b8efb
Michael Ding <yandy.ding@gmail.com>
2022-02-15 22:39:26 +0800

    add lint for fmt

--- fmt-lint.sh
+++ fmt-lint.sh
@@ -5,3 +5,6 @@ find $(pwd)/include -name '*.cc' -exec clang-format -i {} \;
 
 find $(pwd)/source -name '*.h' -exec clang-format -i {} \;
 find $(pwd)/source -name '*.cc' -exec clang-format -i {} \;
+
+cpplint --quiet --recursive $(pwd)/source
+cppcheck --std=c++17 --quiet --enable=performance,portability $(pwd)/source
